95080d7ceaa757cb98e9db8c93ccd19a7446c01a
gtrxAC <gtrxac2@gmail.com>
2026-07-10 23:27:50 +0300

    WBMP parsing

--- src/fi/gtrxac/bluewap/client/WmlImageItem.java
+++ src/fi/gtrxac/bluewap/client/WmlImageItem.java
@@ -6,6 +6,7 @@ import fi.gtrxac.bluewap.ui.*;
 import javax.microedition.lcdui.Font;
 import javax.microedition.lcdui.Image;
 import javax.microedition.lcdui.Graphics;
+import java.io.*;
 
 public class WmlImageItem extends StringItem implements Runnable {
     private URL url;
@@ -42,13 +43,52 @@ public class WmlImageItem extends StringItem implements Runnable {
     }
 
     public void run() {
+        InputStream is = null;
         try {
-            image = HTTP.createRequest(url.toString(false)).getResponseImage();
+            is = HTTP.createRequest(url.toString(false)).getResponseStream();
+            image = parseWbmp(is);
             App.resizeAllScreens();
         }
         catch (Exception e) {
             e.printStackTrace();
         }
+        finally {
+            try { is.close(); } catch (Exception e) {}
+        }
+    }
+
+    private Image parseWbmp(InputStream is) throws Exception {
+        DataInputStream dis = new DataInputStream(is);
+        dis.skip(2);
+
+        int width = dis.readUnsignedByte();
+        if (width == 0 || width > 127) {
+            throw new IllegalArgumentException("unsupported image width");
+        }
+        int height = dis.readUnsignedByte();
+        if (height == 0 || height > 127) {
+            throw new IllegalArgumentException("unsupported image height");
+        }
+
+        int bytesPerRow = (width + 7)/8;
+        int fullWidth = bytesPerRow*8;
+        int[] rgb = new int[width*height];
+
+        for (int y = 0; y < height; y++) {
+            for (int x = 0; x < bytesPerRow; x++) {
+                int b = dis.readUnsignedByte();
+                for (int i = 0; i < 8; i++) {
+                    int destX = x*8 + i;
+                    if (destX >= width) continue;
+
+                    int bit = (b >> (7 - i)) & 1;
+                    rgb[y*width + destX] = (bit == 1 ? 0xFFFFFFFF : 0xFF000000);
+                }
+            }
+        }
+
+        dis.close();
+        return Image.createRGBImage(rgb, width, height, true);
     }
 }
 //#endif
