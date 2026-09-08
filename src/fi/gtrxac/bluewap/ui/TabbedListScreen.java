eec3f3fa0615f75b06fe4a436d39c84680924b4e
gtrxAC <gtrxac2@gmail.com>
2026-09-08 12:46:04 +0300

    UI: tab view drawing and key input

--- src/fi/gtrxac/bluewap/ui/TabbedListScreen.java
+++ src/fi/gtrxac/bluewap/ui/TabbedListScreen.java
@@ -1,19 +1,25 @@
 package fi.gtrxac.bluewap.ui;
 
+import javax.microedition.lcdui.Canvas;
 import javax.microedition.lcdui.Graphics;
 import java.util.*;
+import fi.gtrxac.bluewap.*;
 
 public class TabbedListScreen extends Screen {
     public int activeTabIndex;
     public Vector tabNames;
+    public Vector truncatedTabNames;
     public Vector tabs;
 
-    private int tabsHeight = 100;
+    private static int tabWidth;
+    private static int tabMargin;
+    private static int tabBarHeight;
 
     public TabbedListScreen(int margin) {
         super(margin);
         activeTabIndex = -1;
         tabNames = new Vector();
+        truncatedTabNames = new Vector();
         tabs = new Vector();
     }
 
@@ -22,17 +28,67 @@ public class TabbedListScreen extends Screen {
     }
 
     public void draw(Graphics g) {
-        g.setColor(0xFF0000);
-        g.fillRect(0, 0, getWidth(), getHeight());
+        g.setColor(0xCCCCCC);
+        g.fillRect(0, 0, getWidth(), tabBarHeight);
+
+        drawTabs(g);
 
         ListView tab = getCurrentTab();
         if (tab != null) tab.prepareAndDraw(g);
     }
 
+    private void drawTabs(Graphics g) {
+        int outlineColor = Util.blend(0xDDDDDD, 0x000000, 7);
+        int tabDrawWidth = tabWidth - tabMargin;
+
+        for (int i = 0; i < tabs.size(); i++) {
+            boolean selected = (i == activeTabIndex);
+            String tabName = (String) truncatedTabNames.elementAt(i);
+
+            int tabX = i*tabWidth + tabMargin;
+            int tabY = selected ? tabMargin : Fonts.height/4;
+            int tabHeight = tabBarHeight - tabY;
+
+            g.setColor(selected ? 0xFFFFFF : 0xDDDDDD);
+            g.fillRect(tabX, tabY, tabDrawWidth, tabHeight);
+
+            // outline
+            g.setColor(outlineColor);
+            g.drawRect(tabX, tabY, tabDrawWidth, tabHeight + 1);
+
+            g.setColor(selected ? 0x000000 : 0x111111);
+            g.setFont(Fonts.plain);
+            g.drawString(
+                tabName,
+                tabX + tabDrawWidth/2,
+                tabY + tabHeight/2 - Fonts.height/2,
+                Graphics.HCENTER | Graphics.TOP);
+        }
+
+        // bottom line before and after the currently selected tab
+        int selectedTabBegin = tabMargin + tabWidth*activeTabIndex;
+        int selectedTabEnd = selectedTabBegin + tabWidth - tabMargin;
+        int lineY = tabBarHeight - 1;
+
+        g.setColor(outlineColor);
+        g.drawLine(0, lineY, selectedTabBegin, lineY);
+        g.drawLine(selectedTabEnd, lineY, getWidth(), lineY);
+    }
+
     public void recalc() {
+        tabMargin = Fonts.height/7;
+        tabWidth = (getWidth() - tabMargin)/tabs.size();
+        tabBarHeight = Fonts.height*3/2;
+
+        truncatedTabNames.setSize(0);
+
         for (int i = 0; i < tabs.size(); i++) {
             ListView tab = (ListView) tabs.elementAt(i);
-            tab.setPosition(0, tabsHeight, getWidth(), getHeight() - tabsHeight);
+            tab.setPosition(0, tabBarHeight, getWidth(), getHeight() - tabBarHeight);
+
+            String tabName = (String) tabNames.elementAt(i);
+            String truncatedName = Util.stringToWidth(tabName, Fonts.plain, tabWidth - tabMargin*2);
+            truncatedTabNames.addElement(truncatedName);
         }
     }
 
@@ -61,8 +117,21 @@ public class TabbedListScreen extends Screen {
     }
 
     public void keyEvent(int keyCode, int gameAction) {
-        ListView tab = getCurrentTab();
-        if (tab != null) tab.keyEvent(keyCode, gameAction);
+        if (gameAction == Canvas.LEFT) {
+            activeTabIndex--;
+            if (activeTabIndex < 0) activeTabIndex = tabs.size() - 1;
+            AppBase.repaint();
+        }
+        else if (gameAction == Canvas.RIGHT) {
+            activeTabIndex++;
+            if (activeTabIndex >= tabs.size()) activeTabIndex = 0;
+            AppBase.repaint();
+        }
+        else {
+            // pass the event through to the tab's listview
+            ListView tab = getCurrentTab();
+            if (tab != null) tab.keyEvent(keyCode, gameAction);
+        }
     }
 
     public void pointerPressed(int x, int y) {
